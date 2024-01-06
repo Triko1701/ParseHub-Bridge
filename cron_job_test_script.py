3f18a515cc2a2f30f5706b1b75ca05149b8ad457
Duc Anh Nguyen <129013379+Triko1701@users.noreply.github.com>
2024-01-07 08:22:54 +1100

    edit cron job test script

--- cron_job_test_script.py
+++ cron_job_test_script.py
@@ -1,8 +1,8 @@
-import datetime
+from utils.time import get_time
 
 # Open a file in append mode
-with open('/opt/cron_job_test_script_log.txt', 'a') as file:
+with open('/opt/cron_job_test_file.txt', 'a') as file:
     # Get the current timestamp
-    current_time = datetime.datetime.now()
+    current_time = get_time()
     # Write the timestamp to the file
     file.write(f"Script executed at: {current_time}\n")
