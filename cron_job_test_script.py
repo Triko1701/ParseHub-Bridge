4bf0b4853fb53fc73efffbecb49383c460ad7242
Duc Anh Nguyen <129013379+Triko1701@users.noreply.github.com>
2024-01-07 17:00:12 +1100

    remove test scripts

--- cron_job_test_script.py
+++
@@ -1,8 +0,0 @@
-from utils.time import get_time
-
-# Open a file in append mode
-with open('/opt/cron_job_test_file.txt', 'a') as file:
-    # Get the current timestamp
-    current_time = get_time()
-    # Write the timestamp to the file
-    file.write(f"Script executed at: {current_time}\n")
