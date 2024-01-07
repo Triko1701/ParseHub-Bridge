851a789af503ae38d57bea6d2b1cdd43c8d23c54
Duc Anh Nguyen <129013379+Triko1701@users.noreply.github.com>
2024-01-07 17:00:51 +1100

    debug

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import subprocess
-
 from app import create_app
 
-app = create_app()
-
-
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
-    # subprocess.run(["gunicorn", "-b", "0.0.0.0:5000", "-w", "5", "app:app"])
