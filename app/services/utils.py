42d910156a529672df7ba9bfb7f77e699fc2966d
Duc Anh Nguyen <129013379+Triko1701@users.noreply.github.com>
2024-01-17 18:49:17 +1100

    debug import name and_

--- app/services/utils.py
+++ app/services/utils.py
@@ -1,5 +1,5 @@
 from flask import current_app as c_app
-from sqlalchemy import select, _and
+from sqlalchemy import select, and_
 
 from ..models import Post
 
@@ -10,7 +10,7 @@ def remove_existing_posts(data: dict) -> None:
         for i in range(len(data["job_post"])):
             
             post_exists = db.session.query(select(1).filter(
-                _and(
+                and_(
                     Post.title == data["job_post"][i][Post.title.key],
                     Post.job_url == data["job_post"][i][Post.job_url.key],
                     Post.description == data["job_post"][i][Post.description.key]
